b6aa8bbf8cb52369711d9aec243d0b9179d881de
Personal <personal@Ankits-MacBook-Pro.local>
2025-05-29 20:42:22 +0530

    Change local font to CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        orbitron: ['Orbitron', 'sans-serif'], // Make Orbitron available via Tailwind
-        inter: ['Inter', 'sans-serif'],
+        orbitron: ["Orbitron", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
